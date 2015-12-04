e92c496035051b9fb4c76308b9e953935ff004aa
Bartdd3 <leandroandrele@gmail.com>
2015-12-04 09:38:25 -0200

    Initial commit

---
+++ app/src/main/java/com/arrobatecinformatica/registro/tela.java
@@ -0,0 +1,16 @@
+package com.arrobatecinformatica.registro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Matrix on 03/12/2015.
+ */
+public class tela extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela);
+    }
+}
